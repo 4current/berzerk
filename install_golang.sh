2e05884a21f7564480570ca6f7c2b525ad7479aa
Richard Morrisey <rich.morrisey@gmail.com>
2019-10-23 09:13:22 -0400

    first time consumer connect

--- install_golang.sh
+++ install_golang.sh
@@ -3,3 +3,6 @@ set -x
 ansible-playbook -vv --ask-become-pass -i ./hosts golang.yml
 
 /usr/local/go/bin/go get -u github.com/ThreeDotsLabs/watermill
+
+/usr/local/go/bin/go get -u github.com/Shopify/sarama
+/usr/local/go/bin/go get -u github.com/ThreeDotsLabs/watermill-kafka/pkg/kafka
